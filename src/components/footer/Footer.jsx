c0acd003067fbedfa99bfed76e458be328f97060
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-12-02 22:05:45 +0530

    added: footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,19 +1,121 @@
-import React from "react";
+import logo from "../../assets/logo.png";
+import { CiInstagram } from "react-icons/ci";
+import { FaLinkedin, FaFacebook, FaYoutube, FaTwitter } from "react-icons/fa";
+import FooterLinks from "../../utils/footerLinks";
+
+const socials = [
+    {
+        id: 1,
+        icon: CiInstagram,
+        link: "https://www.instagram.com/notionhq/",
+        hover: "#d62976",
+    },
+    {
+        id: 2,
+        icon: FaTwitter,
+        link: "https://twitter.com/NotionHQ",
+        hover: "#1DA1F2",
+    },
+    {
+        id: 3,
+        icon: FaLinkedin,
+        link: "https://www.linkedin.com/company/notionhq/",
+        hover: "#0077b5",
+    },
+    {
+        id: 4,
+        icon: FaFacebook,
+        link: "https://www.facebook.com/NotionHQ/",
+        hover: "#316FF6",
+    },
+    {
+        id: 5,
+        icon: FaYoutube,
+        link: "https://www.youtube.com/channel/UCoSvlWS5XcwaSzIcbuJ-Ysg",
+        hover: "#CD201F",
+    },
+];
 
 const Footer = () => {
     return (
-        <footer className="mx-4 border-t-[1px] py-16 px-20">
-            <div>
-                <div className="mx-6 sm:m-0 ">
-                    <select
-                        id="language"
-                        className="bg-transparent text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[150px] p-2.5 "
-                    >
-                        <option value="English">English, USA</option>
-                        <option value="Hindi">Hindi</option>
-                        <option value="Arabic">Arabic</option>
-                        <option value="Romania">Romania</option>
-                    </select>
+        <footer className="mx-4 border-t-[1px] py-16 px-6 md:px-[100px]">
+            <div className="flex items-start flex-col md:flex-row justify-center gap-6">
+                <div className="w-[20%] flex flex-col gap-6 items-start">
+                    {/* logo */}
+                    <div>
+                        <a
+                            href="https://www.notion.so/product"
+                            className="flex items-center gap-2"
+                        >
+                            <img src={logo} alt="logo" className="w-9 h-9" />
+                            <span className="font-bold text-lg">Notion</span>
+                        </a>
+                    </div>
+
+                    {/* social icons */}
+                    <div className="flex gap-1">
+                        {socials?.map((item) => {
+                            return (
+                                <span
+                                    key={item.id}
+                                    className="text-[22px] cursor-pointer p-1 hover:bg-gray-200 group rounded-md"
+                                >
+                                    <a href={item.link}>
+                                        <item.icon
+                                            className={`group-hover:text-[${item.hover}] text-gray-500`}
+                                        />
+                                    </a>
+                                </span>
+                            );
+                        })}
+                    </div>
+                    <div className="mx-2 sm:m-0 ">
+                        <select
+                            id="language"
+                            className="bg-gray-200 text-gray-900 text-sm rounded-lg ring-blue-500 border-blue-500 block w-[150px] px-4 py-3 "
+                        >
+                            <option value="English">English, USA</option>
+                            <option value="Hindi">Hindi</option>
+                            <option value="Arabic">Arabic</option>
+                            <option value="Romania">Romania</option>
+                            <option value="Deutsch">Deutsch</option>
+                        </select>
+                    </div>
+                </div>
+
+                {/* links section */}
+                <div className="flex-1 flex flex-col gap-4 mt-6 md:mt-0">
+                    <div className="columns-2 lg:columns-3">
+                        {FooterLinks?.map((item, i) => {
+                            return (
+                                <div key={i} className="">
+                                    <span className="text-md font-semibold">
+                                        {item.tag}
+                                    </span>
+                                    <ul className="pl-[2px] flex flex-col gap-1 mt-2 mb-8">
+                                        {item?.Links.map((it, i) => {
+                                            return (
+                                                <li
+                                                    key={i}
+                                                    className="text-gray-400 font-[500] hover:underline hover:text-blue-500"
+                                                >
+                                                    <a href={it.link}>
+                                                        {it.name}
+                                                    </a>
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <div className="">
+                        <p className="text-sm">We do not sell or share your personal information</p>
+                        <span className="text-gray-400 text-[14px]">
+                            © {new Date().getFullYear()} Notion Labs, Inc.
+                        </span>
+                    </div>
                 </div>
             </div>
         </footer>
---
+++ src/utils/footerLinks.js
@@ -0,0 +1,224 @@
+const FooterLinks = [
+    {
+        tag: "Product",
+        Links: [
+            {
+                name: "Wikis",
+                link: "https://www.notion.so/product/wikis",
+            },
+            {
+                name: "Projects",
+                link: "https://www.notion.so/product/projects",
+            },
+            {
+                name: "Docs",
+                link: "https://www.notion.so/product/docs",
+            },
+            {
+                name: "Notion AI",
+                link: "https://www.notion.so/product/ai",
+            },
+            {
+                name: "What's new",
+                link: "https://www.notion.so/releases",
+            },
+        ],
+    },
+    {
+        tag: "Solutions",
+        Links: [
+            {
+                name: "Enterprise",
+                link: "https://www.notion.so/enterprise",
+            },
+            {
+                name: "Small business",
+                link: "https://www.notion.so/teams",
+            },
+            {
+                name: "Personal use",
+                link: "https://www.notion.so/personal",
+            },
+            {
+                name: "Remote work",
+                link: "https://www.notion.so/remote",
+            },
+            {
+                name: "Startups",
+                link: "https://www.notion.so/startups",
+            },
+            {
+                name: "Education",
+                link: "https://www.notion.so/product/notion-for-education",
+            },
+            {
+                name: "Nonprofits",
+                link: "https://www.notion.so/nonprofits",
+            },
+            {
+                name: "Engineering",
+                link: "https://www.notion.so/product/notion-for-engineering",
+            },
+            {
+                name: "Product",
+                link: "https://www.notion.so/product/notion-for-product",
+            },
+            {
+                name: "Design",
+                link: "https://www.notion.so/product/notion-for-design",
+            },
+            {
+                name: "Managers",
+                link: "https://www.notion.so/product/notion-for-managers",
+            },
+        ],
+    },
+    {
+        tag: "Download",
+        Links: [
+            {
+                name: "iOS & Android",
+                link: "https://www.notion.so/mobile",
+            },
+            {
+                name: "Mac & Windows",
+                link: "https://www.notion.so/desktop",
+            },
+            {
+                name: "Web Clipper",
+                link: "https://www.notion.so/web-clipper",
+            },
+        ],
+    },
+
+    {
+        tag: "Build",
+        Links: [
+            {
+                name: "Integrations",
+                link: "https://www.notion.so/integrations",
+            },
+            {
+                name: "Templates",
+                link: "https://www.notion.so/templates",
+            },
+            {
+                name: "API docs",
+                link: "https://developers.notion.com/",
+            },
+            {
+                name: "Guides & Tutorials",
+                link: "https://www.notion.so/help/guides",
+            },
+            {
+                name: "Hire a consultant",
+                link: "hhttps://www.notion.so/consultants",
+            },
+            {
+                name: "Become an affiliate",
+                link: "https://www.notion.so/affiliates",
+            },
+        ],
+    },
+    {
+        tag: "Learn",
+        Links: [
+            {
+                name: "Customer stories",
+                link: "https://www.notion.so/integrations",
+            },
+            {
+                name: "Help center",
+                link: "https://www.notion.so/templates",
+            },
+            {
+                name: "Webinars",
+                link: "https://developers.notion.com/",
+            },
+            {
+                name: "Blog",
+                link: "https://www.notion.so/help/guides",
+            },
+            {
+                name: "Community",
+                link: "hhttps://www.notion.so/consultants",
+            },
+        ],
+    },
+    {
+        tag: "Get Started",
+        Links: [
+            {
+                name: "Switch from Confluence",
+                link: "https://www.notion.so/confluence",
+            },
+            {
+                name: "Switch from Asana",
+                link: "https://www.notion.so/compare-against/comparison-notion-vs-asana",
+            },
+            {
+                name: "Switch from Evernote",
+                link: "https://www.notion.so/evernote",
+            },
+            {
+                name: "Compare vs Monday",
+                link: "https://www.notion.so/compare-against/notion-vs-monday",
+            },
+            {
+                name: "Compare vs Clickup",
+                link: "https://www.notion.so/compare-against/notion-vs-clickup",
+            },
+            {
+                name: "Compare vs Jira",
+                link: "https://www.notion.so/compare-against/notion-vs-jira",
+            },
+        ],
+    },
+    {
+        tag: "Resources",
+        Links: [
+            {
+                name: "Pricing",
+                link: "https://www.notion.so/pricing",
+            },
+            {
+                name: "About us",
+                link: "https://www.notion.so/templates",
+            },
+            {
+                name: "Careers",
+                link: "https://developers.notion.com/",
+            },
+            {
+                name: "Media kit",
+                link: "https://www.notion.so/help/guides",
+            },
+            {
+                name: "Email us",
+                link: "hhttps://www.notion.so/consultants",
+            },
+            {
+                name: "Security",
+                link: "https://www.notion.so/affiliates",
+            },
+            {
+                name: "Cookie settings",
+                link: "https://www.notion.so/affiliates",
+            },
+            {
+                name: "Terms & privacy",
+                link: "https://www.notion.so/affiliates",
+            },
+            {
+                name: "California Privacy Notice",
+                link: "https://www.notion.so/affiliates",
+            },
+            {
+                name: "Status",
+                link: "https://www.notion.so/affiliates",
+            },
+        ],
+    },
+];
+
+export default FooterLinks;
